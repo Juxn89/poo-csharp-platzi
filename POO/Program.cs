b384dabd9619fc7457287423f44693a26a8a75f3
Juan Gómez <gomezj01@stepstone.com>
2024-02-21 13:30:08 -0600

    Primera clase

---
+++ POO/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var apuntador = new Apuntador();
+Apuntador apuntadorPlatzi = new Apuntador();
+Apuntador apuntadorPlatzi2 = new Apuntador();
+
+apuntadorPlatzi.Color = "Negro";
+apuntadorPlatzi2.Color = "Blanco";
+
+class Apuntador
+{
+    public string Color { get; set; }
+    public double Large { get; set; }
+    public short CantOfBotton { get; set; }
+    public bool HasStickers { get; set; }
+}
