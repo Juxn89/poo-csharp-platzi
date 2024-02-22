b7208e73cde96e0423d98ea07c604fd659ef9956
Juan Gómez <gomezj01@stepstone.com>
2024-02-22 09:34:34 -0600

    Clases complejas

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,5 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
 Console.WriteLine("Hello, World!");
+
+var batmanPower1 = new SuperPower();
+batmanPower1.Name = "Rich";
+batmanPower1.Description = "💰";
+batmanPower1.Level = LevelPower.LevelThree;
+
+var batmanPower2 = new SuperPower();
+batmanPower2.Name = "Smart";
+batmanPower2.Description = "Smart | Description";
+batmanPower2.Level = LevelPower.LevelThree;
 
 var batman = new SuperHero();
 
@@ -8,7 +20,7 @@ batman.Name = "Batman";
 batman.CanFly = false;
 batman.RealName = "Bruce Wayne";
 batman.City = "Gotham";
-batman.Powers = new[] { "Rich" };
+batman.Powers = new List<SuperPower>() { batmanPower1, batmanPower2};
 batman.CanFly = false;
 
 class SuperHero
@@ -17,6 +29,20 @@ class SuperHero
     public string Name { get; set; }
     public string RealName { get; set; }
     public string City { get; set; }
-    public string[] Powers { get; set; }
+    public List<SuperPower> Powers { get; set; }
     public bool CanFly { get; set; }
+}
+
+class SuperPower
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public LevelPower Level { get; set; }
+}
+
+enum LevelPower
+{
+    LevelOne = 1,
+    LevelTwo = 2, 
+    LevelThree = 3
 }
